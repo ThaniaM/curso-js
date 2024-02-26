d6a0036f04ff552657e7d9055a309062ad5ee0b3
Thania Cruz <tmeritzel.cb@gmail.com>
2024-02-26 02:47:28 -0600

    tipos de datos

---
+++ valores.js
@@ -0,0 +1,17 @@
+40 //numero
+"Thania" //texto
+
+true  
+false  //booleanos
+
+null
+undefined //valores reservados para algun valor faltante
+
+//valores tipo objetos
+
+[1,2,3]     //arrays (objeto)
+{nombre: "Thania"}  //objetos literales
+
+typeof 40 //tipo de dato
+
+
