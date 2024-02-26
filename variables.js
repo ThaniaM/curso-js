ec436aee450e0527db2d15077d91accbd96194ed
Thania Cruz <tmeritzel.cb@gmail.com>
2024-02-26 02:58:13 -0600

    variables js

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Oscar";
+
+//declarar
+var edad; //se declara la variable pero no se asigna un valor
+//inicializar
+edad = 30;
+
+edad;
+
+var elementos = ["Computadora", "Celular"];
+elementos[1]; //trae el elemento 1 
+
+var persona = {
+  nombre: "Diego",
+  edad: 30
+}
+persona;
